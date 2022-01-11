af6f9aa30a8245ddbc8ff5d1a56f73cd88108887
Guillaume MODARD <guillaumemodard@gmail.com>
2022-01-11 08:59:05 +0100

    Création du Sample V5

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+
+	<AppWrapper
+		:cfg="cfg"
+		:cfg-menu="cfgMenu"
+		:cfg-slots="cfgSlots">
+
+		<template v-slot:header>
+		</template>
+
+
+		<template v-slot:menu>
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</template>
+
+		<template v-slot:list>
+		</template>
+
+		<template v-slot:core>
+			<router-view/>
+		</template>
+
+	</AppWrapper>
+	
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+import AppWrapper from '@/components/pebble-ui/AppWrapper.vue'
+
+export default {
+
+	data() {
+		return {
+			cfg: {},
+			cfgMenu: {},
+			cfgSlots: {}
+		}
+	},
+
+	components: {
+		AppWrapper
+	}
+
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
